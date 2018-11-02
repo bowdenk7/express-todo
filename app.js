276266f1a6455a895ca0390891f174c4df492439
Bowden Kelly <wilkelly@microsoft.com>
2018-11-02 15:30:48 -0700

    woooo

--- app.js
+++ app.js
@@ -33,6 +33,6 @@ app.post("/removetask", function(req, res) {
   res.redirect("/");
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000, function() {
   console.log("Example app listening on port 3000!");
 });
